c24922c89521b367cffb3e11805cc28e6346d575
otaviobonini <otgbonini@gmail.com>
2026-01-18 13:57:22 -0300

    redirecionando aos planos suaves e tambem retirei o botao FALAR NO WHATSAPP

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
   return (
     <div className='divmenu'>
         <img className='logo' src={Logo} alt="Logo" width="120" />
-        <button onClick={() => window.open("https://api.whatsapp.com/send?phone=554898175758&text=Oii%2C%20vim%20pelo%20site%20e%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20da%20LCKR", "_blank")} className='buttonzap'><i className="fa-brands fa-whatsapp"></i> FALAR NO WHATSAPP</button>
+        
         <div className='textoDiv'>
         <p className='texto'>Crossfit em criciúma é na LCKR.</p>
         <h1 className='textoPrincipal'>SUPERE SEUS LIMITES</h1>
